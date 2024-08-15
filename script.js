fa9fb4b3a43675dda34d51ff4e0ba4b25ca5dc87
Joao <silvasouzajoaovictor877@gmail.com>
2024-08-15 16:10:38 -0300

    Portfolio AlphaTec versão 1

---
+++ script.js
@@ -0,0 +1,13 @@
+/*evento de clique a todos os link com # quando clicado correspondente a uma página de com efeito deslizante*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
